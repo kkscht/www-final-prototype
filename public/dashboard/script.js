6035e03f2d56c92e378ab0b25a13583c2384b2af
Charlotte Le <charlottele@berkeley.edu>
2024-04-24 23:10:40 -0700

    dashboard working bio persistence, interactable tags

--- public/dashboard/script.js
+++ public/dashboard/script.js
@@ -3,8 +3,15 @@ document.getElementById('submit').addEventListener('click', function(event){
   event.preventDefault();  // Prevent the form from submitting traditionally
 
   // Save all form data now
-  localStorage.setItem('userName', document.getElementById('first-name').value);
+  localStorage.setItem('firstName', document.getElementById('first-name').value);
+  localStorage.setItem('lastName', document.getElementById('last-name').value);
+  localStorage.setItem('phoneNumber', document.getElementById('phone-number').value);
+  localStorage.setItem('travelInformation', document.getElementById('travel-information').value);
+  localStorage.setItem('bio', document.getElementById('bio').value);
+  localStorage.setItem('hobbies', document.getElementById('hobbies').value);
+  localStorage.setItem('interests', document.getElementById('interests').value);
   localStorage.setItem('profilePicture', window.tempProfilePic || localStorage.getItem('profilePicture'));
+
 
   // Additional inputs can also be saved here
   // localStorage.setItem('lastName', document.getElementById('last-name').value);
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,12 +15,44 @@ navLinks.forEach(function(navLink) {
 
 
 
-
 window.onload = () => {
-       
+
+    
+
+    const firstName = localStorage.getItem('firstName');
+    const lastName = localStorage.getItem('lastName');
+    const phoneNumber = localStorage.getItem('phoneNumber');
+    const travelInformation = localStorage.getItem('travelInformation');
+    const bio = localStorage.getItem('bio');
+    const hobbies = localStorage.getItem('hobbies');
+    const interests = localStorage.getItem('interests');
     const profilePicture = localStorage.getItem('profilePicture');
+    
+
     if (profilePicture) {
-      document.querySelector('.profile-pic').src = profilePicture;
+        document.querySelector('.profile-pic').src = profilePicture;
+    }
+
+    if (firstName) {
+        document.getElementById('full-name').innerHTML = firstName;
+    }
+    if (lastName) {
+        document.getElementById('full-name').innerHTML += ' ' + lastName;
+    }
+    // if (phoneNumber) {
+    //     document.getElementById('phone-number').innerHTML = phoneNumber;
+    // }
+    if (travelInformation) {
+        document.getElementById('travel-information').innerHTML = travelInformation;
+    }
+    if (bio) {
+        document.getElementById('bio').innerHTML = bio;
+    }
+    if (hobbies) {
+        document.getElementById('hobbies').innerHTML = hobbies;
+    }
+    if (interests) {
+        document.getElementById('interests').innerHTML = interests;
     }
   };
 
